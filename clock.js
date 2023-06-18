50a27a45f723cc052693f5069232ec9c68f82c04
Razia Munir <irazia.munir@gmail.com>
2023-06-18 21:52:55 +0500

    optimized

--- clock.js
+++ clock.js
@@ -11,8 +11,9 @@ setInterval(
         document.querySelector('#minutes').style.transform = `rotateZ(${minutes * 6}deg)`;
         document.querySelector('#seconds').style.transform = `rotateZ(${seconds * 6}deg)`;
 
-        document.querySelector('#hour').innerHTML = (hours < 10) ? '0' + hours : hours;
-        document.querySelector('#minute').innerHTML = (minutes < 10) ? '0' + minutes : minutes;
-        document.querySelector('#secnd').innerHTML = (seconds < 10) ? '0' + seconds : seconds;
+        let digitalClock = document.querySelector('#digitalClock');
+        digitalClock.children[0].innerHTML = (hours < 10) ? '0' + hours : hours;
+        digitalClock.children[1].innerHTML = (minutes < 10) ? '0' + minutes : minutes;
+        digitalClock.children[2].innerHTML = (seconds < 10) ? '0' + seconds : seconds;
     }
 );
