fd27c830cc4806f420695594e025759c4492b26e
Razia Munir <irazia.munir@gmail.com>
2023-06-18 21:13:42 +0500

    time pins width updated

--- clock.js
+++ clock.js
@@ -1,15 +1,18 @@
-setInterval(()=>{
-    let timestamp = new Date();
+setInterval(
+    () =>
+    {
+        let timestamp = new Date();
 
-    let hours = timestamp.getHours();
-    let minutes = timestamp.getMinutes();
-    let seconds = timestamp.getSeconds();
+        let hours = timestamp.getHours();
+        let minutes = timestamp.getMinutes();
+        let seconds = timestamp.getSeconds();
 
-    document.querySelector('#hours').style.transform = `rotateZ(${ (hours * 30) + ((minutes * 6) / 12)}deg)`;
-    document.querySelector('#minutes').style.transform = `rotateZ(${minutes * 6}deg)`;
-    document.querySelector('#seconds').style.transform = `rotateZ(${seconds * 6}deg)`;
+        document.querySelector('#hours').style.transform = `rotateZ(${ (hours * 30) + ((minutes * 6) / 12)}deg)`;
+        document.querySelector('#minutes').style.transform = `rotateZ(${minutes * 6}deg)`;
+        document.querySelector('#seconds').style.transform = `rotateZ(${seconds * 6}deg)`;
 
-    document.querySelector('#hour').innerHTML = hours;
-    document.querySelector('#minute').innerHTML = minutes;
-    document.querySelector('#secnd').innerHTML = seconds;
-});
+        document.querySelector('#hour').innerHTML = (hours < 10) ? '0' + hours : hours;
+        document.querySelector('#minute').innerHTML = (minutes < 10) ? '0' + minutes : minutes;
+        document.querySelector('#secnd').innerHTML = (seconds < 10) ? '0' + seconds : seconds;
+    }
+);
