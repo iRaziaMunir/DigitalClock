a4606525bc5a1ed237b01c0f72e1c796c8dca183
Razia Munir <irazia.munir@gmail.com>
2023-06-18 19:01:12 +0500

    js code  separated

---
+++ clock.js
@@ -0,0 +1,15 @@
+setInterval(()=>{
+    let timestamp = new Date();
+
+    let hours = timestamp.getHours();
+    let minutes = timestamp.getMinutes();
+    let seconds = timestamp.getSeconds();
+
+    document.querySelector('#hours').style.transform = `rotateZ(${ (hours * 30) + ((minutes * 6) / 12)}deg)`;
+    document.querySelector('#minutes').style.transform = `rotateZ(${minutes * 6}deg)`;
+    document.querySelector('#seconds').style.transform = `rotateZ(${seconds * 6}deg)`;
+
+    document.querySelector('#hour').innerHTML = hours;
+    document.querySelector('#minute').innerHTML = minutes;
+    document.querySelector('#secnd').innerHTML = seconds;
+});
